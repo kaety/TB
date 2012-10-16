ddbc2565d1798150bd91dc7187fe12c6ce42526e
Kristofer Tapper <tapperk@ed-3354-05.studat.chalmers.se>
2012-10-16 15:15:15 +0200

    AuthBB and logut

---
+++ TBookWeb/src/main/java/edu/chl/TBookWeb/AuthBB.java
@@ -0,0 +1,32 @@
+package edu.chl.TBookWeb;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Named("auth")
+@RequestScoped
+public class AuthBB {
+
+    private static final Logger log = Logger.getLogger(AuthBB.class.getName());
+
+    public String logout() {
+        String result = "/login?faces-redirect=true";
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            log.log(Level.SEVERE, "Failed to logout user!", e);
+            result = "/loginfailed?faces-redirect=true";
+        }
+
+        return result;
+    }
+}
